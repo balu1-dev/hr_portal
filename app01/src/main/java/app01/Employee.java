9e4e463e4d0669e3226ba54761aeab99c3691a34
balu1-dev <pegademo600@gmail.com>
2024-10-20 20:33:44 +0530

    first commit

---
+++ app01/src/main/java/app01/Employee.java
@@ -0,0 +1,56 @@
+package app01;
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table
+public class Employee {
+	@Id
+	private Integer eid;
+	private String ename;
+	private Double esal;
+	@Column(updatable = false)
+	@CreationTimestamp
+	private LocalDate createTime;
+	@Column(updatable = true)
+	@UpdateTimestamp
+	private LocalDate UpdateTime;
+	public Employee() {	
+	}
+	public Employee(Integer eid, String ename, Double esal) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.esal = esal;
+	}
+	public Integer getEid() {
+		return eid;
+	}
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Double getEsal() {
+		return esal;
+	}
+	public void setEsal(Double esal) {
+		this.esal = esal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esal=" + esal + "]";
+	}
+}
